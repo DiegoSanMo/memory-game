9777d9a423a42cc9cf9cae7aba25feb86964a21e
Diego Mosan <diego14290764@itcg.edu.mx>
2018-09-16 16:22:36 -0500

    The 'num' property was added to the image object.

--- src/providers/general-config/general-config.ts
+++ src/providers/general-config/general-config.ts
@@ -8,6 +8,7 @@ import { Injectable } from '@angular/core';
 */
 
 export interface IImage{
+  num: number;
   name: string;
   frontUrl: string;
   backUrl: string;
@@ -24,120 +25,140 @@ export class GeneralConfigProvider {
   finaArray:any;
   images: Array<IImage> = [
     {
+      num: 0,
       name: 'Amsterdam',
       frontUrl: '',
       backUrl: '../../assets/pictures/back.jpg',
       visible: false
     },
     {
+      num: 1,
       name: 'Düsseldorf',
       frontUrl: '',
       backUrl: '../../assets/pictures/back.jpg',
       visible: false
     },
     {
+      num: 2,
       name: 'Vancouver',
       frontUrl: '',
       backUrl: '../../assets/pictures/back.jpg',
       visible: false
     },
     {
+      num: 3,
       name: 'Barcelona',
       frontUrl: '',
       backUrl: '../../assets/pictures/back.jpg',
       visible: false
     },
     {
+      num: 4,
       name: 'Lisboa',
       frontUrl: '',
       backUrl: '../../assets/pictures/back.jpg',
       visible: false
     },
     {
+      num: 5,
       name: 'Hong Kong',
       frontUrl: '',
       backUrl: '../../assets/pictures/back.jpg',
       visible: false
     },
     {
+      num: 6,
       name: 'Fukuoka',
       frontUrl: '',
       backUrl: '../../assets/pictures/back.jpg',
       visible: false
     },
     {
+      num: 7,
       name: 'Helsinki',
       frontUrl: '',
       backUrl: '../../assets/pictures/back.jpg',
       visible: false
     },
     {
+      num: 8,
       name: 'Kioto',
       frontUrl: '',
       backUrl: '../../assets/pictures/back.jpg',
       visible: false
     },
     {
+      num: 9,
       name: 'Estocolmo',
       frontUrl: '',
       backUrl: '../../assets/pictures/back.jpg',
       visible: false
     },
     {
+      num: 10,
       name: 'Madrid',
       frontUrl: '',
       backUrl: '../../assets/pictures/back.jpg',
       visible: false
     },
     {
+      num: 11,
       name: 'Hamburgo',
       frontUrl: '',
       backUrl: '../../assets/pictures/back.jpg',
       visible: false
     },
     {
+      num: 12,
       name: 'Zúrich',
       frontUrl: '',
       backUrl: '../../assets/pictures/back.jpg',
       visible: false
     },
     {
+      num: 13,
       name: 'Sidney',
       frontUrl: '',
       backUrl: '../../assets/pictures/back.jpg',
       visible: false
     },
     {
+      num: 14,
       name: 'Copenhague',
       frontUrl: '',
       backUrl: '../../assets/pictures/back.jpg',
       visible: false
     },
     {
+      num: 15,
       name: 'Melbourne',
       frontUrl: '',
       backUrl: '../../assets/pictures/back.jpg',
       visible: false
     },
     {
+      num: 16,
       name: 'Múnich',
       frontUrl: '',
       backUrl: '../../assets/pictures/back.jpg',
       visible: false
     },
     {
+      num: 17,
       name: 'Berlín',
       frontUrl: '',
       backUrl: '../../assets/pictures/back.jpg',
       visible: false
     },
     {
+      num: 18,
       name: 'Viena',
       frontUrl: '',
       backUrl: '../../assets/pictures/back.jpg',
       visible: false
     },
     {
+      num: 19,
       name: 'Tokio',
       frontUrl: '',
       backUrl: '../../assets/pictures/back.jpg',
